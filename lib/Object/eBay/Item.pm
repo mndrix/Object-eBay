7958f11297d1f1b7e0c175b5edd85f333ba1f720
michael <michael@5cb99f66-e806-0410-bcd3-b1d9210a1b92>
2006-03-12 01:38:42 +0000

    Implemented a rudimentary Object::eBay::Item class.
    git-svn-id: svn://tameion/projects/Object-eBay/trunk@1197 5cb99f66-e806-0410-bcd3-b1d9210a1b92

--- lib/Object/eBay/Item.pm
+++ lib/Object/eBay/Item.pm
@@ -1,9 +1,26 @@
 package Object::eBay::Item;
-
-use warnings;
-use strict;
-
 our $VERSION = '0.0.1';
+
+use Class::Std; {
+    use warnings;
+    use strict;
+    use base qw( Object::eBay );
+
+    __PACKAGE__->simple_attributes(qw{
+        Country
+        Title
+        Quantity
+    });
+
+    __PACKAGE__->complex_attributes({
+        Seller => {
+            class => 'User',
+        },
+        Description => {
+            DetailLevel => 'ItemReturnDescription',
+        },
+    });
+}
 
 1;
 
@@ -11,7 +28,7 @@ __END__
 
 =head1 NAME
  
-Object::eBay::Item - <One line description of module's purpose>
+Object::eBay::Item - Represents an item listed on eBay
  
  
 =head1 VERSION
@@ -20,60 +37,56 @@ This documentation refers to Object::eBay::Item version 0.0.1
  
  
 =head1 SYNOPSIS
- 
+
+    use Object::eBay;
     use Object::eBay::Item;
-    # Brief but working code example(s) here showing the most common usage(s)
- 
-    # This section will be as far as many users bother reading
-    # so make it as educational and exemplary as possible.
-  
-  
+
 =head1 DESCRIPTION
- 
+
 A full description of the module and its features.
 May include numerous subsections (i.e. =head2, =head3, etc.) 
- 
- 
+
+
 =head1 SUBROUTINES/METHODS 
- 
+
 A separate section listing the public components of the module's interface. 
 These normally consist of either subroutines that may be exported, or methods
 that may be called on objects belonging to the classes that the module provides.
 Name the section accordingly.
- 
+
 In an object-oriented module, this section should begin with a sentence of the 
 form "An object of this class represents...", to give the reader a high-level
 context to help them understand the methods that are subsequently described.
- 
- 
+
+
 =head1 DIAGNOSTICS
- 
+
 A list of every error and warning message that the module can generate
 (even the ones that will "never happen"), with a full explanation of each 
 problem, one or more likely causes, and any suggested remedies.
- 
- 
+
+
 =head1 CONFIGURATION AND ENVIRONMENT
- 
+
 Object::eBay::Item requires no configuration files or environment variables.
- 
+
 =head1 DEPENDENCIES
- 
+
 A list of all the other modules that this module relies upon, including any
 restrictions on versions, and an indication whether these required modules are
 part of the standard Perl distribution, part of the module's distribution,
 or must be installed separately.
- 
- 
+
+
 =head1 INCOMPATIBILITIES
- 
+
 A list of any modules that this module cannot be used in conjunction with.
 This may be due to name conflicts in the interface, or competition for 
 system or program resources, or due to internal limitations of Perl 
 (for example, many modules that use source code filters are mutually 
 incompatible).
- 
- 
+
+
 =head1 BUGS AND LIMITATIONS
 
 Please report any bugs or feature requests to
@@ -114,11 +127,11 @@ L<http://search.cpan.org/dist/Object-eBay>
 
 =head1 AUTHOR
 
-Michael Hendricks  <michael@palmcluster.org>
+Michael Hendricks  <michael@ndrix.org>
 
 =head1 LICENSE AND COPYRIGHT
  
-Copyright (c) 2006 Michael Hendricks (<michael@palmcluster.org>). All rights
+Copyright (c) 2006 Michael Hendricks (<michael@ndrix.org>). All rights
 reserved.
 
 This program is free software; you can redistribute it and/or modify it
